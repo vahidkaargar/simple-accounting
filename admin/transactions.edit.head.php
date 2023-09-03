80901118a027a408ece72cfc00387a8c3bc176cf
hossein mokhtaripour <48912145+hosseinmokhtaripour@users.noreply.github.com>
2023-09-03 21:37:43 +0330

    show transactions of an ID

--- admin/transactions.edit.head.php
+++ admin/transactions.edit.head.php
@@ -1,1 +1,34 @@
 <?php
+
+$title = 'لیست خریدهای کاربر';
+
+$_ENV['error'] = '';
+
+$telegramId = mysqli_real_escape_string(DB, $_GET['telegram_id'] ?? 0);
+
+//make query & get data
+$query = mysqli_query(DB, "SELECT * FROM `transactions` where `telegram_id`='$telegramId'");
+
+// check for exists
+if (mysqli_num_rows($query) === 0) {
+    die('<h1>Wallet not exists!</h1>');
+}
+//fetch the result as an array
+$transactions = mysqli_fetch_all($query, MYSQLI_ASSOC);
+
+//free result from memory
+mysqli_free_result($query);
+
+
+//write query for database
+$sql = "SELECT * FROM wallets where `telegram_id`='$telegramId'";
+
+//make query & get data
+$query = mysqli_query(DB, $sql);
+
+//fetch the result as an array
+$user = mysqli_fetch_assoc($query);
+
+//free result from memory
+mysqli_free_result($query);
+
--- admin/transactions.edit.php
+++ admin/transactions.edit.php
@@ -1,3 +1,29 @@
 <h2 class="border-bottom pb-2 mt-2">
     لیست خریدهای کاربر
 </h2>
+<div class="py-3">
+    <table class="table table-striped text-center">
+        <thead>
+        <tr>
+            <th scope="col">تلگرام آیدی</th>
+            <th scope="col">تلگرام یوزرنیم</th>
+            <th scope="col">تراکنش</th>
+            <th scope="col">سرور</th>
+            <th scope="col">تاریخ ایجاد</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($transactions as $transaction): ?>
+            <tr>
+                    <th scope="row" class="ltr"><?php echo htmlspecialchars($transaction['telegram_id']); ?></th>
+                    <td class="ltr">
+                        <a class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+                           href="./admin.php?action=transactions.change&telegram_id=<?= htmlspecialchars($transaction['telegram_id']) ?>"><?= htmlspecialchars($user['telegram_username']) ?></a>
+                    </td>
+                    <td class="ltr"><?php echo number_format($transaction['amount']); ?></td>
+                    <td class="ltr"><?php echo htmlspecialchars($transaction['servers']); ?></td>
+                    <td class="ltr"><?= $transaction['created_at'] ?></td>
+                </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
