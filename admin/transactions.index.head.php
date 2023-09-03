0fe4119fc65f52f7fdca0e5778a9131844211831
hossein mokhtaripour <48912145+hosseinmokhtaripour@users.noreply.github.com>
2023-09-03 21:35:14 +0330

    add show list for transactions

--- admin/transactions.index.head.php
+++ admin/transactions.index.head.php
@@ -1,3 +1,25 @@
 <?php
+$title = 'لیست خریدها';
+//write query for database
+$sql = "SELECT * FROM transactions ORDER BY created_at";
 
-$title = 'لیست تراکنش ها';
+//make query & get data
+$query = mysqli_query(DB, $sql);
+
+//fetch the result as an array
+$transactions = mysqli_fetch_all($query, MYSQLI_ASSOC);
+
+//free result from memory
+mysqli_free_result($query);
+
+//write query for database
+$sql = "SELECT * FROM wallets ORDER BY created_at";
+
+//make query & get data
+$query = mysqli_query(DB, $sql);
+
+//fetch the result as an array
+$users = mysqli_fetch_all($query, MYSQLI_ASSOC);
+
+//free result from memory
+mysqli_free_result($query);
