a851309eeb79344a0694ddc0cb880284b9f5a617
Vahid Kaargar <vahidkaargar@gmail.com>
2023-09-05 14:04:38 +0330

    add search form for transactions and render pages

--- admin/transactions.index.php
+++ admin/transactions.index.php
@@ -1,35 +1,71 @@
 <h2 class="border-bottom pb-2 mt-2">
-     لیست خریدها
+    لیست خریدها
 </h2>
 <div class="py-3">
-    <table class="table table-striped text-center">
+    <div class="border-bottom pb-3 mb-4">
+        <form class="row g-3" action="./admin.php" method="get">
+            <input type="hidden" name="action" value="transactions.index">
+            <div class="col-md-4">
+                <label for="telegram_username" class="form-label">یوزرنیم تلگرام</label>
+                <input name="telegram_username" type="text" class="form-control ltr" id="telegram_username">
+            </div>
+            <div class="col-md-2">
+                <label for="transaction_type" class="form-label">نوع تراکنش</label>
+                <select name="transaction_type" id="transaction_type" class="form-select">
+                    <option value="0">همه</option>
+                    <option value="1">واریز</option>
+                    <option value="2">برداشت</option>
+                </select>
+            </div>
+            <div class="col-md-6 pt-4">
+                <button type="submit" class="btn btn-primary mt-2">جستجو</button>
+            </div>
+        </form>
+    </div>
+    <table class="table table-hover text-center">
         <thead>
         <tr>
-            <th scope="col">تلگرام آیدی</th>
+            <th scope="col">مبلغ</th>
+            <th style="width: 120px" scope="col">کد تراکنش</th>
             <th scope="col">تلگرام یوزرنیم</th>
-            <th scope="col">تراکنش</th>
+            <th scope="col">مشاهده</th>
             <th scope="col">سرور</th>
             <th scope="col">تاریخ ایجاد</th>
         </tr>
         </thead>
         <tbody>
         <?php foreach ($transactions as $transaction): ?>
-            <?php foreach ($users as $user): ?>
-                <?php if($user['telegram_id'] == $transaction['telegram_id']): ?>
-                    <?php break; ?>
-                <?php endif; ?>
-            <?php endforeach; ?>
+            <?php $bgColor = ($transaction['amount'] < 0) ? 'bg-light-danger' : 'bg-light-success'; ?>
             <tr>
-                    <th scope="row" class="ltr"><?php echo htmlspecialchars($transaction['telegram_id']); ?></th>
-                    <td class="ltr">
-                        <a class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
-                           href="./admin.php?action=transactions.edit&telegram_id=<?= htmlspecialchars($transaction['id']) ?>"><?= htmlspecialchars($user['telegram_username']) ?></a>
-                    </td>
-                    <td class="ltr"><?php echo number_format($transaction['amount']); ?></td>
-                    <td class="ltr"><?php echo htmlspecialchars($transaction['server']); ?></td>
-                    <td class="ltr"><?= $transaction['created_at'] ?></td>
-                </tr>
+                <td class="text-start ltr <?= $bgColor ?>"><?php echo number_format($transaction['amount']); ?></td>
+                <th scope="row" class="ltr <?= $bgColor ?>"><?php echo $transaction['id']; ?></th>
+                <td class="ltr <?= $bgColor ?>">
+                    <a class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+                       target="_blank"
+                       href="./admin.php?action=wallets.edit&telegram_id=<?= htmlspecialchars($transaction['telegram_id']) ?>">
+                        <?= htmlspecialchars($transaction['telegram_username']) ?>
+                    </a>
+                </td>
+                <td class="ltr <?= $bgColor ?>">
+                    <a class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+                       target="_blank"
+                       href="./admin.php?action=transactions.edit&id=<?= $transaction['id'] ?>">مشاهده</a>
+                </td>
+                <td class="ltr <?= $bgColor ?>"><?php echo htmlspecialchars($transaction['server'] ?? 'N/A'); ?></td>
+                <td class="ltr <?= $bgColor ?>"><?= $transaction['created_at'] ?></td>
+            </tr>
         <?php endforeach; ?>
         </tbody>
     </table>
+
+    <nav class="ltr">
+        <ul class="pagination justify-content-center">
+            <?php foreach ($paginate['page_numbers'] as $page_number) : ?>
+                <li class="page-item">
+                    <a class="page-link"
+                       href="/admin.php?action=transactions.index&page=<?= $page_number ?>"><?= $page_number ?></a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </nav>
 </div>
