3d966f156a2fd5fc64adcc34dfb5139a086a527a
Vahid Kaargar <vahidkaargar@gmail.com>
2023-09-05 14:03:47 +0330

    join transactions and wallets and paginate them

--- admin/transactions.index.head.php
+++ admin/transactions.index.head.php
@@ -1,25 +1,20 @@
 <?php
 $title = 'لیست خریدها';
+
+
+// generate paginate
+$transactionsCount = mysqli_fetch_assoc(mysqli_query(DB, "SELECT COUNT(*) AS total FROM transactions"));
+$paginate = paginate($transactionsCount['total']);
+
+
 //write query for database
-$sql = "SELECT * FROM transactions ORDER BY created_at";
+$sql = "SELECT t.*, w.telegram_username FROM transactions t INNER JOIN wallets w ON t.telegram_id = w.telegram_id LIMIT {$paginate['per_page']} OFFSET {$paginate['offset']}";
 
 //make query & get data
 $query = mysqli_query(DB, $sql);
 
 //fetch the result as an array
 $transactions = mysqli_fetch_all($query, MYSQLI_ASSOC);
-
-//free result from memory
-mysqli_free_result($query);
-
-//write query for database
-$sql = "SELECT * FROM wallets ORDER BY created_at";
-
-//make query & get data
-$query = mysqli_query(DB, $sql);
-
-//fetch the result as an array
-$users = mysqli_fetch_all($query, MYSQLI_ASSOC);
 
 //free result from memory
 mysqli_free_result($query);
