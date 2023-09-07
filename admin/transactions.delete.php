58419900b102594c01cd5937c9250550b33de526
hossein mokhtaripour <48912145+hosseinmokhtaripour@users.noreply.github.com>
2023-09-07 17:31:33 +0330

    create transactions.delete & transactions.delete.head files for delete a transactions

---
+++ admin/transactions.delete.php
@@ -0,0 +1,7 @@
+<h2 class="border-bottom pb-2 mt-2">
+   حذف تراکنش
+</h2>
+<div class="py-3">
+   <h4><?php echo $operationStatus; ?></h4><br>
+   <a href="./admin.php?action=transactions.index" class="btn btn-primary">بازگشت</a>
+</div>
