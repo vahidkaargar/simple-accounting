ac8f0f39f1f35a52223bea1ba5f262ac363a7b2f
Vahid Kaargar <vahidkaargar@gmail.com>
2023-09-17 13:22:33 +0330

    Refactor transaction.delete

--- admin/transactions.delete.head.php
+++ admin/transactions.delete.head.php
@@ -1,16 +1,16 @@
 <?php
    $title = 'حذف تراکنش';
 
+$_ENV['message'] = '';
 
-   $id = mysqli_real_escape_string(DB, $_POST['id'] ?? 0);
-   
-   $sql = "UPDATE `transactions` SET `is_paid`=0, `updated_at`=NOW() WHERE `id`=$id ";
-   
-   //save to db check
-   if (mysqli_query(DB, $sql)) {
-      $operationStatus = "تراکنش موردنظر با موفقیت حذف شد.";
-     //redirect('./admin.php?action=transactions.index');
-   } else {
-      $operationStatus = "عملیات موردنظر انجام نشد.";
-      echo mysqli_error(DB);
-   }
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id = mysqli_real_escape_string(DB, $_POST['id'] ?? 0);
+    //save to db check
+    if (mysqli_query(DB, "UPDATE `transactions` SET `is_paid`=0, `updated_at`=NOW() WHERE `id`='$id'")) {
+        $_ENV['message'] = "تراکنش موردنظر با موفقیت حذف شد.";
+        //redirect('./admin.php?action=transactions.index');
+    } else {
+        $_ENV['message'] = "عملیات موردنظر انجام نشد.";
+        echo mysqli_error(DB);
+    }
+}
--- admin/transactions.delete.php
+++ admin/transactions.delete.php
@@ -1,7 +1,7 @@
 <h2 class="border-bottom pb-2 mt-2">
-   حذف تراکنش
+    <?=$title?>
 </h2>
 <div class="py-3">
-   <h4><?php echo $operationStatus; ?></h4><br>
-   <a href="./admin.php?action=transactions.index" class="btn btn-primary">بازگشت</a>
+    <h4><?= $_ENV['message'] ?></h4><br>
+    <a href="./admin.php?action=transactions.index" class="btn btn-primary">بازگشت</a>
 </div>
