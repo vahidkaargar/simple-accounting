773c40f775b5ca5315dfde2d5de14485a8857223
Vahid Kaargar <vahidkaargar@gmail.com>
2023-09-17 13:22:13 +0330

    refactor transactions.create

--- admin/transactions.create.head.php
+++ admin/transactions.create.head.php
@@ -1,21 +1,32 @@
 <?php
-   $title = 'شارژ کیف پول';
+$title = 'شارژ کیف پول';
+$_ENV['error'] = '';
+$telegramId = mysqli_real_escape_string(DB, $_REQUEST['telegram_id'] ?? 0);
 
-   $telegramId = mysqli_real_escape_string(DB, $_GET['telegram_id'] ?? 0);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-   $amount = mysqli_real_escape_string(DB, $_POST['amount'] ?? 0);
+    $query = mysqli_query(DB, "SELECT * FROM `wallets` where `telegram_id`='$telegramId'");
+    if (mysqli_num_rows($query) === 0) {
+        die('<h1>Wallet not exists!</h1>');
+    }
+    mysqli_free_result($query);
 
-   if ($amount) {
-       //create sql
-       $sql = sprintf(
-           "INSERT INTO `transactions` (`telegram_id`, `amount`, `created_at`, `updated_at`) VALUES ('%s', '%s', NOW(), NOW())",
-           $telegramId, $amount
-       );
 
-       //save to db check
-       if (mysqli_query(DB, $sql)) {
-           redirect('./admin.php?action=wallets.edit&telegram_id=' . $telegramId);
-       } else {
-           echo mysqli_error(DB);
-       }
-   }
+    $amount = mysqli_real_escape_string(DB, $_REQUEST['amount'] ?? 0);
+    if ($amount >= ADD_FUND_MIN) {
+        //create sql
+        $sql = sprintf(
+            "INSERT INTO `transactions` (`telegram_id`, `amount`, `created_at`, `updated_at`) VALUES ('%s', '%s', NOW(), NOW())",
+            $telegramId, $amount
+        );
+
+        //save to db check
+        if (mysqli_query(DB, $sql)) {
+            redirect('./admin.php?action=wallets.edit&telegram_id=' . $telegramId);
+        } else {
+            $_ENV['error'] = mysqli_error(DB);
+        }
+    } else {
+        $_ENV['error'] = 'مبلغ باید بیشتر از حداقل باشد.';
+    }
+}
--- admin/transactions.create.php
+++ admin/transactions.create.php
@@ -1,15 +1,23 @@
 <h2 class="border-bottom pb-2 mt-2">
-  شارژ کیف پول
+    شارژ کیف پول
 </h2>
 <div class="py-3">
-  <div class="m-auto ltr" style="max-width: 400px">
-      <form class="" action="./admin.php?action=transactions.create&telegram_id=<?= $telegramId ?>" method="post">
-          <div class="input-group mb-3 ">
-              <input type="text" class="form-control" aria-label="Sizing example input"
-                     aria-describedby="inputGroup-sizing-default" name="amount">
-              <span class="input-group-text" id="inputGroup-sizing-default">مبلغ</span>
-          </div>
-          <button type="submit" class="btn btn-primary">افزایش کیف پول</button>
-      </form>
-  </div>
+    <div class="m-auto ltr" style="max-width: 400px">
+        <form action="./admin.php" method="post">
+            <?php if ($_ENV['error']): ?>
+                <div class="alert alert-danger rtl"><?= $_ENV['error'] ?></div>
+            <?php endif; ?>
+            <div class="input-group mb-3 ">
+                <input type="hidden" name="action" value="transactions.create">
+                <input type="hidden" name="telegram_id" value="<?= $telegramId ?>">
+                <input type="text" class="form-control" name="amount" value="<?= $amount ?? '' ?>" autofocus
+                       placeholder="حداقل: <?= number_format(ADD_FUND_MIN) ?>">
+                <span class="input-group-text" id="inputGroup-sizing-default">مبلغ</span>
+            </div>
+            <div><small>
+
+                </small></div>
+            <button type="submit" class="btn btn-primary">افزایش کیف پول</button>
+        </form>
+    </div>
 </div>
