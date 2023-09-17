f0d4c4fda97aac33400e08167ec262c931390178
Vahid Kaargar <vahidkaargar@gmail.com>
2023-09-17 13:26:24 +0330

    change username to service_username

--- api/transactions.update.php
+++ api/transactions.update.php
@@ -2,7 +2,7 @@
 
 $transactionId = mysqli_real_escape_string(DB, $_REQUEST['transaction_id'] ?? 0);
 $server = mysqli_real_escape_string(DB, $_REQUEST['server']);
-$username = mysqli_real_escape_string(DB, $_REQUEST['username']);
+$serviceUsername = mysqli_real_escape_string(DB, $_REQUEST['service_username']);
 
 
 $query = mysqli_query(DB, "SELECT * FROM `transactions` WHERE `id`='$transactionId'");
@@ -15,7 +15,7 @@ if (mysqli_num_rows($query) === 0) {
 $transaction = mysqli_fetch_assoc($query);
 mysqli_free_result($query);
 
-if (mysqli_query(DB, sprintf("UPDATE `transactions` SET `server`='%s', `service_username`='%s' WHERE `id`='%s'", $server, $username, transactionId))) {
+if (mysqli_query(DB, sprintf("UPDATE `transactions` SET `server`='%s', `service_username`='%s' WHERE `id`='%s'", $server, $serviceUsername, $transactionId))) {
     die(json_encode([
         "success" => true,
         "message" => 'Transaction updated.'
